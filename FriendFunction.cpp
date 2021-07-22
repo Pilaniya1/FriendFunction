ae36d304bcb313b96cd8630c39af82d886295e0d
Vinay Pilaniya <82079006+Pilaniya1@users.noreply.github.com>
2021-07-22 16:54:35 +0530

    Add files via upload
    -> How to use friend function.
    -> We will declare friend function inside the class. and access outside the class by using (.)dot operator.

---
+++ FriendFunction.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class sample
+{
+    int a;
+    int b;
+    public:
+    void setvalue(){a=25;b=40;}
+    friend float mean(sample s);
+
+};
+float mean(sample s)
+{
+    return float(s.a +s.b)/2.0;
+}
+int main()
+{
+    sample X;//object x
+    X.setvalue();
+    cout<<"Mean value = "<<mean(X)<<"\n";
+    return 0;
+}
